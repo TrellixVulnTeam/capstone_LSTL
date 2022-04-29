eb9b6e992891d3768981b4c43f3ee73990835324
Francis Guido <fjguido97@gmail.com>
2022-04-29 17:53:19 -0400

    installed scss

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import axios from 'axios'
 import PlanetCard from './PlanetCard';
-import './Home.css'
+
 
 
 
