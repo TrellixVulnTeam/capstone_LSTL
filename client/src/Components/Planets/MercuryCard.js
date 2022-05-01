8062658dc8dc21fde478f1dd3623b7500b3727ac
Francis Guido <fjguido97@gmail.com>
2022-04-30 20:37:01 -0400

    started mercury folder

---
+++ client/src/Components/Planets/MercuryCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom';
+
+//change to each planet ID
+
+function MercuryCard({ mercury }) {
+
+    const { id, image, about, name } = mercury
+    return (
+        <div>
+            <Link to={`/planets/1`}> 
+                <img src={image} />
+            </Link>
+        </div>
+    )
+}
+
+export default MercuryCard;
---
+++ client/src/Components/Planets/MercuryDetail.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import MemoForm from './MemoForm';
+import MemoCard from './MemoCard';
+import axios from 'axios';
+
+function PlanetDetail({ user }) {
+
+  const [Mercury, setMercury] = useState([])
+  const { id } = useParams();
+  const [memo, setMemo] = useState([])
+
+  // useEffect(() => {
+
+  // }, [])
+
+
+
+
+  useEffect(() => {
+    console.log(id)
+    fetch(`/planets/1`)
+    //change to each planet ID
+      .then((r) => r.json())
+      .then((data) => setMercury(data))
+    fetchData()
+  }, [id])
+
+  async function fetchData() {
+    axios.get('/notes')
+    .then(r => setMemo(r.data.filter(note => note.planet_id == id) )
+
+  
+    )}
+
+  console.log(id);
+  const { name, about, image } = planet
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      <img src={image} class="center"/>
+      <h3>{about}</h3>
+      <MemoForm user={user} planet={planet} />
+      {memo.map((m) => {
+        return (<MemoCard
+          memo={m}
+          key={m.id}
+
+        />)
+      })}
+
+    </div>
+  )
+}
+
+export default PlanetDetail
